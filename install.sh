7d6ad5d43d524b2f2a7b12b5750e84c5a624d54a
User Name <you@example.com>
2023-02-10 15:52:02 -0600

    Updated install script and added todo features

--- install.sh
+++ install.sh
@@ -9,15 +9,21 @@ mkdir -p $HOME/.local/share/todo/notes
 cp todo $HOME/.local/bin/todo
 chmod 755 $HOME/.local/bin/todo
 
-echo -n "What shell do you have?: "
+echo -n "What shell do you have?[bash/zsh/Cancel] "
 read shell
 case $shell in
     "zsh")
-        echo -n 'TODO_DIR=$HOME/.local/share/todo/notes' >> $HOME/.zshrc
+        echo -n 'export TODO_DIR="$HOME/.local/share/todo/notes"' >> $HOME/.zshrc
+	echo -n 'export TODO_DEFAULT=$(date +%Y.%m.%d.md)' >> $HOME/.zshrc
+
         ;;
     "bash")
-        echo -n 'TODO_DIR=$HOME/.local/share/todo/notes' >> $HOME/.bashrc
+        echo -n 'export TODO_DIR="$HOME/.local/share/todo/notes"' >> $HOME/.bashrc
+	echo -n 'export TODO_DEFAULT=$(date +%Y.%m.%d.md)' >> $HOME/.bashrc
         ;;
+    "cancel")
+	echo "EXiting..."
+	;;
     *)
         echo "Sorry, not understood or shell not supported by this install script.
         you will need to set the '\$TODO_HOME' variable yourself"
